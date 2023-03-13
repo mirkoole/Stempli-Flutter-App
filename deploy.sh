95375ef1d281fc1c071f0815f0044e1a3c852612
Mirko Oleszuk <mirko@oleszuk.de>
2023-03-13 20:22:43 +0100

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
+# deploy web-app to github pages
 rm -r docs
-
 flutter clean
-
 flutter build web --base-href /Stempli-Flutter-App/
-
 mv build/web docs
-
 git commit -am "deploy web"
-
 git push
+
+# build android app
+flutter build apk
+flutter build apk --split-per-abi
+
