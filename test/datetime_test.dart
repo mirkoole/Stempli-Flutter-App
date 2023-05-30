c175f74fb6a81cf978a833463ed716d6e9d4a942
Mirko Oleszuk <mirko@oleszuk.de>
2023-05-30 23:35:32 +0200

    update datetime tests

--- test/datetime_test.dart
+++ test/datetime_test.dart
@@ -3,21 +3,31 @@ import 'package:test/test.dart';
 
 void main() {
   group('date time ', () {
-    test('calcWeeklyWorkTimeToDailyWorktime', () {
-      expect(calcWeeklyWorkTimeToDailyWorktime(40, 5), 8.0);
-      expect(calcWeeklyWorkTimeToDailyWorktime(30, 5), 6.0);
-      expect(calcWeeklyWorkTimeToDailyWorktime(38, 5), 7.6);
+    test('getAdjustIntervalMinute', () {
+      expect(getAdjustIntervalMinute(0), "");
+      expect(getAdjustIntervalMinute(60), "1 Min");
+      expect(getAdjustIntervalMinute(600), "10 Min");
+      expect(getAdjustIntervalMinute(3600), "60 Min");
+    });
 
-      expect(calcWeeklyWorkTimeToDailyWorktime(32, 4), 8.0);
+    test('convertDurationToSeconds', () {
+      expect(convertDurationToSeconds(const Duration(minutes: 1)), 60);
+      expect(convertDurationToSeconds(const Duration(hours: 1)), 60 * 60);
+      expect(convertDurationToSeconds(const Duration(hours: 2)), 2 * 60 * 60);
+      expect(convertDurationToSeconds(const Duration(hours: 1, minutes: 30)),
+          90 * 60);
+      expect(convertDurationToSeconds(const Duration(hours: 8, minutes: 0)),
+          8 * 60 * 60);
     });
 
     test('getDailyWorkTimeString', () {
-      expect(getDailyWorkTimeString(8), '8:00 h');
-      expect(getDailyWorkTimeString(7), '7:00 h');
-      expect(getDailyWorkTimeString(6), '6:00 h');
+      expect(getDailyWorkTimeString(8 * 60 * 60), '8:00 h');
+      expect(getDailyWorkTimeString(7 * 60 * 60), '7:00 h');
+      expect(getDailyWorkTimeString(6 * 60 * 60), '6:00 h');
 
-      expect(getDailyWorkTimeString(7.6), '7:36 h');
-      expect(getDailyWorkTimeString(7.5), '7:30 h');
+      expect(getDailyWorkTimeString(60 * 36 + 7 * 60 * 60), '7:36 h');
+      expect(getDailyWorkTimeString(60 * 30 + 7 * 60 * 60), '7:30 h');
+      expect(getDailyWorkTimeString(60 * 15), '0:15 h');
     });
   });
 }
