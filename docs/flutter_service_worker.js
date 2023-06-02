b56ddb46d8b6f285bb89d3e0ab5f8cacdda29da6
Mirko Oleszuk <mirko@oleszuk.de>
2023-06-02 23:34:35 +0200

    deploy web

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "cf678034fe654f1ef342634a7f1373b9",
-"index.html": "85f527c96e76fee1c7831909e351716a",
-"/": "85f527c96e76fee1c7831909e351716a",
-"main.dart.js": "81bb1bce5fd33fd8bfb6702ca387fedd",
+const RESOURCES = {"version.json": "fe2dad33846f2178799b7ea1b0559975",
+"index.html": "304e1706892f464f8e934b3d6128251b",
+"/": "304e1706892f464f8e934b3d6128251b",
+"main.dart.js": "f9460ad9b0fbf45988d5967066af48b2",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "2d2a8a4340bfb6e0693ee93d996de101",
 "icons/Icon-192.png": "eff7c36e87ffa83138211eb4779a53ca",
