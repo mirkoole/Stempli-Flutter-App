c38cff3af7da382df9d7d1e0dc2e66ef6a87f102
Mirko Oleszuk <mirko@codepunks.net>
2025-03-21 15:05:19 +0100

    add exit

--- deploy_apk.sh
+++ deploy_apk.sh
@@ -5,7 +5,7 @@ flutter build apk
 flutter build apk --split-per-abi
 flutter build appbundle
 
-cd build/app/intermediates/merged_native_libs/release/out/lib
+cd build/app/intermediates/merged_native_libs/release/out/lib || exit
 zip -r native_debug_symbols.zip arm64-v8a armeabi-v7a x86_64
 mv native_debug_symbols.zip ~/Downloads
-cd -
+cd - || exit
