8d85e51f16239f0130995eeb5b4d6160a6ff830e
Mirko Oleszuk <mirko@codepunks.net>
2025-06-20 14:03:55 +0200

    update path

--- deploy_apk.sh
+++ deploy_apk.sh
@@ -3,9 +3,11 @@
 # build android app
 flutter build apk
 flutter build apk --split-per-abi
-flutter build appbundle
+#flutter build appbundle
 
-cd build/app/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib || exit
-zip -r native_debug_symbols.zip arm64-v8a armeabi-v7a x86_64
-mv native_debug_symbols.zip ~/Downloads
-cd - || exit
+#cd build/app/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib || exit
+#zip -r native_debug_symbols.zip arm64-v8a armeabi-v7a x86_64
+#mv native_debug_symbols.zip ~/Downloads
+#cd - || exit
+
+open build/app/outputs/flutter-apk
